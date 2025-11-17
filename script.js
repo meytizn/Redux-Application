03b8e70d8e367a4918e5eda7468edff1aaef6e93
meytizn <zamanianmehdi23desktop@gmail.com>
2025-11-17 15:24:50 +0330

    store created in s3

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert('project started  ')
+const store = {
+
+    state: {
+        cart:[] , shop:[]
+    } ,
+
+
+
+    getState (){
+        return this.state
+    } ,
+
+
+    dispatch (action){
+        return this.state.cart.push(action)
+    }
+
+}
+
+
+store.dispatch("apple")
+console.log(store.getState())
